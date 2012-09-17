8e40921fc9875f881ec59e2e6196523a43973fef
songrit <songrit@gmail.com>
2012-09-17 11:27:59 +0700

    upgrade jquery 1.7.1, fix font Sarabun

--- lib/generators/gmindapp/templates/app/controllers/gmindapp_controller.rb
+++ lib/generators/gmindapp/templates/app/controllers/gmindapp_controller.rb
@@ -7,7 +7,8 @@ class GmindappController < ApplicationController
   end
   def index
     if login?
-      @xmains= Gmindapp::Xmain.all.also_in(:status=>['R','I']).order("created_at")
+      @xmains= Gmindapp::Xmain.where(status:'R').union.where(status:'I').asc(:created_at)
+      # @xmains= Gmindapp::Xmain.all.also_in(:status=>['R','I']).order("created_at")
     end
     render :layout => false 
   end
