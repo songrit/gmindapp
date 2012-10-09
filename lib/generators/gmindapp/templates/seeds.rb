79ff6eee95191484be7889cce4a3c56df15ad751
songrit <songrit@gmail.com>
2012-10-09 14:31:04 +0700

    fix menu, seeds

--- lib/generators/gmindapp/templates/seeds.rb
+++ lib/generators/gmindapp/templates/seeds.rb
@@ -1,4 +1,6 @@
-identity= Identity.create :name => "admin", :email => "admin@test.com", :password => "secret",
-  :password_confirmation => "secret"
-User.create :provider => "identity", :uid => identity.id.to_s, :name => identity.name,
-  :email => identity.email, :role => "M,A,D"
+if Identity.count==0
+  identity= Identity.create :name => "admin", :email => "admin@test.com", :password => "secret",
+    :password_confirmation => "secret"
+  User.create :provider => "identity", :uid => identity.id.to_s, :name => identity.name,
+    :email => identity.email, :role => "M,A,D"
+end
