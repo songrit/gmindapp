7b9463f29c2d814d3e6b0fcfd6ce346fea1fa559
songrit <songrit@gmail.com>
2012-10-11 00:55:59 +0700

    FormBuilder for ActionView::Helpers

--- lib/gmindapp/helpers.rb
+++ lib/gmindapp/helpers.rb
@@ -76,6 +76,7 @@ module Gmindapp
         out += "<span class='steps_more'>#{i}</span>"
       end
       out += "</div>"
+      out.html_safe
     end
 
     # old methods, don't know where they came from
@@ -294,5 +295,114 @@ class String
 #    s
     code, name = s.split(':')
     code.downcase.strip.gsub(' ','_').gsub(/[^#_\/a-zA-Z0-9]/,'')
+  end
+end
+
+module ActionView
+  module Helpers
+    class FormBuilder
+#      def date_select_thai(method)
+#        self.date_select method, :use_month_names=>THAI_MONTHS, :order=>[:day, :month, :year]
+#      end
+      def date_field(method, options = {})
+        default= self.object.send(method) || Date.today
+        data_options= ({"mode"=>"calbox"}).merge(options)
+        out= %Q(<input name='#{self.object_name}[#{method}]' id='#{self.object_name}_#{method}' value='#{default.strftime("%F")}' type='date' data-role='datebox' data-options='#{data_options.to_json}'>)
+        out.html_safe
+      end
+      def time_field(method, options = {})
+        default= self.object.send(method) || Time.now
+        data_options= ({"mode"=>"timebox"}).merge(options)
+        out=%Q(<input name='#{self.object_name}[#{method}]' id='#{self.object_name}_#{method}' value='#{default}' type='date' data-role='datebox' data-options='#{data_options.to_json}'>)
+        out.html_safe
+      end
+      def date_select_thai(method, default= Time.now, disabled=false)
+        date_select method, :default => default, :use_month_names=>THAI_MONTHS, :order=>[:day, :month, :year], :disabled=>disabled
+      end
+      def datetime_select_thai(method, default= Time.now, disabled=false)
+        datetime_select method, :default => default, :use_month_names=>THAI_MONTHS, :order=>[:day, :month, :year], :disabled=>disabled
+      end
+
+      def point(o={})
+        o[:zoom]= 11 unless o[:zoom]
+        o[:width]= '500px' unless o[:width]
+        o[:height]= '300px' unless o[:height]
+        if o[:lat].blank?
+          o[:lat] = 37.5
+          o[:lng] = -95
+          o[:zoom] = 4
+        end
+
+        out = <<-EOT
+  <script type='text/javascript'>
+  //<![CDATA[
+    var latLng;
+    var map_#{self.object_name};
+    var marker_#{self.object_name};
+
+    function initialize() {
+      var lat =  #{o[:lat]};
+      var lng =  #{o[:lng]};
+      //var lat =  position.coords.latitude"; // HTML5 pass position in function initialize(position)
+      // google.loader.ClientLocation.latitude;
+      //var lng =  position.coords.longitude;
+      // google.loader.ClientLocation.longitude;
+      latLng = new google.maps.LatLng(lat, lng);
+      map_#{self.object_name} = new google.maps.Map(document.getElementById("map_#{self.object_name}"), {
+        zoom: #{o[:zoom]},
+        center: latLng,
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+      });
+      marker_#{self.object_name} = new google.maps.Marker({
+        position: latLng,
+        map: map_#{self.object_name},
+        draggable: true,
+      });
+      google.maps.event.addListener(marker_#{self.object_name}, 'dragend', function(event) {
+        $('##{self.object_name}_lat').val(event.latLng.lat());
+        $('##{self.object_name}_lng').val(event.latLng.lng());
+      });
+      google.maps.event.addListener(map_#{self.object_name}, 'click', function(event) {
+        $('##{self.object_name}_lat').val(event.latLng.lat());
+        $('##{self.object_name}_lng').val(event.latLng.lng());
+        move();
+      });
+      $('##{self.object_name}_lat').val(lat);
+      $('##{self.object_name}_lng').val(lng);
+
+    };
+
+
+    function move() {
+      latLng = new google.maps.LatLng($('##{self.object_name}_lat').val(), $('##{self.object_name}_lng').val());
+      map_#{self.object_name}.panTo(latLng);
+      marker_#{self.object_name}.setPosition(latLng);
+    }
+
+    google.maps.event.addDomListener(window, 'load', initialize);
+    //if (navigator.geolocation) {
+    // navigator.geolocation.getCurrentPosition(initialize);
+    //} else {
+    // google.maps.event.addDomListener(window, 'load', no_geo);
+    //}
+
+
+  //]]>
+  </script>
+  Latitude: #{self.text_field :lat, :style=>"width:100px;" }
+  Longitude: #{self.text_field :lng, :style=>"width:100px;" }
+  <p/>
+  <div id='map_#{self.object_name}' style='width:#{o[:width]}; height:#{o[:height]};'></div>
+  <script>
+    $('##{self.object_name}_lat').change(function() {move()})
+    $('##{self.object_name}_lng').change(function() {move()})
+    var w= $("input[id*=lat]").parent().width();
+    $("input[id*=lat]").css('width',w/4.5+'px')
+    $("input[id*=lng]").css('width',w/4.5+'px')
+  </script>
+EOT
+        out.html_safe
+      end
+    end
   end
 end
