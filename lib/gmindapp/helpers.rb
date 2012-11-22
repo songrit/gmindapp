a6ce336900c1a29802a7dee722b497c8e97f305b
songrit <songrit@gmail.com>
2012-11-22 15:05:09 +0700

    clean up sidebar

--- lib/gmindapp/helpers.rb
+++ lib/gmindapp/helpers.rb
@@ -152,15 +152,15 @@ module Gmindapp
     def ui_action?(s)
       %w(form output mail pdf).include? s
     end
-    def handle_gma_notice
-      if Gmindapp::Notice.recent.count>0
-        notice= Gmindapp::Notice.recent.last
-        notice.update_attribute :unread, false
-        "<script>notice('#{notice.message}');</script>"
-      else
-        ""
-      end
-    end
+    # def handle_gma_notice
+    #   if Gmindapp::Notice.recent.count>0
+    #     notice= Gmindapp::Notice.recent.last
+    #     notice.update_attribute :unread, false
+    #     "<script>notice('#{notice.message}');</script>"
+    #   else
+    #     ""
+    #   end
+    # end
     def process_services
       # todo: persist mm_md5
       xml= @app||get_app
